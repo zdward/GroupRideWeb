48aedc91ccd0efa9e1c2bdd4e6d957358033c5c0
alexistj <josepa2@rpi.edu>
2019-04-26 01:18:30 -0400

    ReadMe update, server page routes, gitignore update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # GroupRideWeb
 RPI GroupRide Web App
+
+The application acts as a social network. Students can make posts about an Uber/Lyft ride they intend to take (and at what time), notifying RPI students in their area. These notified students then have the option to join the Uber/Lyft group (a.k.a the GroupRide) and communicate with the individuals in the group.
+
+
+To run our application:
+
+1. Clone the repositiory
+    -download zip file at: https://github.com/zdward/GroupRideWeb.git
+    - clone from git from terminal
+        - If you do not have git installed on your device: https://git-scm.com/downloads
+        - From terminal, go to your desired directory and use git clone https://github.com/zdward/GroupRideWeb.git
+    -Use prefered IDE git platform
+        
+2. Install the necessary modules by using 'npm install'
+    - If you do not have node or npm installed please visit https://nodejs.org/en/, and download accordingly
+    - Traverse to the js file in the GroupRideWeb local repository located at /public/js
+    - use npm install
+    
+3. Set environment variables for the database within mongo atlus.
+    - To make an account: https://www.mongodb.com/cloud/atlas
+    - Make a database and an collection called users
+    - Using your user password and username create a .env file so you may access you newly made database
+    - Resources for mongo atlus https://docs.atlas.mongodb.com/getting-started/
+4. Then run the server.js by using nodemon server.js or node server.js within the same public/js file
+5. Navigate to http://localhost:3000 in your browser to get started!
--- public/js/server.js
+++ public/js/server.js
@@ -1,21 +1,57 @@
 // DIRECTORY: GroupRideWeb/public
 // RUN COMMAND: npm start
-// You should see message "Server listening on port 4000"
+// You should see message "Server listening on port 3000"
+const express    = require('express');
+const bodyParser = require('body-parser');
+const path       = require('path');
+const request    = require('request');
+const fs         = require('fs');
+const mongoose 	 = require('mongoose');
+const cors       = require('cors');
+var app = express();
+require('dotenv').config();
+var router = express.Router();
 
-const express = require("express");
+
+// viewed pages view at http://localhost:3000
+ app.use(express.static(path.join(__dirname + '/../../views/')));
+
+
+// Configure Mongooose and MongoDBAtlas connection
+const uri = `mongodb+srv://${ process.env.DBUSER }:${ process.env.DBPASSWORD }@cluster0-xu8sb.mongodb.net/test?retryWrites=true`;
+
+
+//Connection with mongoDB 
+mongoose.connect(uri,
+  {useNewUrlParser: true,
+   useFindAndModify : false,
+   useCreateIndex : true,
+   dbName: 'groupRide'});
+const db = mongoose.connection;
+
+// Configure the app
+app.set('secret', process.env.SECRET); // secret variable
+
+//Used for catching error messages 
+process.on('uncaughtException', function (err) {
+  // This should not happen
+  console.log("Something unexpected happened. This should be handled more gracefully. The culprit is: ", err);
+});
 
 // We might need to use CORS. If so run: npm install cors
 // const cors = require("cors");
 
-app = express();
+
+
 
 // If using cors
 // app.use(cors());
 
-const port = 4000;
+const port = 3000;
 
-// ADD DATABASE CONNECTION
+
+
 
 app.listen(port, () => {
-  console.log("Server listening on port 4000");
+  console.log("Server listening on port 3000");
 });
